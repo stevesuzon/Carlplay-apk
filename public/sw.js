371a0b994332526b0f424e05cc1dc6af78a3954b
stevesuzon <appli.suzon@gmail.com>
2026-09-23 20:35:18 +0200

    V406 : renouvelle le cache fiches marché verrouillées

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const VERSION = "V405-CUSTOM-DEPARTMENT-PICKER";
-const BASE_CACHE = "couteau-suisse-module-base-v405-custom-department-picker";
+const VERSION = "V406-ADMIN-MARKET-FORMS-LOCK";
+const BASE_CACHE = "couteau-suisse-module-base-v406-admin-market-forms-lock";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v366-admin-fresh";
 const ADMIN_CACHE = "couteau-suisse-module-admin-v367-market-full-review";
