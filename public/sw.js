6f3898ba6e736ed6982cadf90e1c67476b177100
stevesuzon <appli.suzon@gmail.com>
2026-09-23 23:50:09 +0200

    V424 : force l’installation de la dernière version

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const VERSION = "V423-VOICE-REOPEN-ADDRESS";
-const BASE_CACHE = "couteau-suisse-module-base-v423-voice-reopen-address";
+const VERSION = "V424-DEPLOY-LATEST";
+const BASE_CACHE = "couteau-suisse-module-base-v424-deploy-latest";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v366-admin-fresh";
 const ADMIN_CACHE = "couteau-suisse-module-admin-v367-market-full-review";
