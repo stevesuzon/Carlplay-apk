a2775b24edbbc1c7ea87f6027fe196234524e5d5
stevesuzon <appli.suzon@gmail.com>
2026-09-25 21:15:09 +0200

    V495: renouveler le cache du moteur vocal simplifié

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const VERSION = "V494-FORCE-NEW-VOICE-FILE";
-const BASE_CACHE = "couteau-suisse-runtime-code-v494";
+const VERSION = "V495-SIMPLE-VOICE";
+const BASE_CACHE = "couteau-suisse-runtime-code-v495";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v439-direct";
 const ADMIN_CACHE = "couteau-suisse-module-admin-v439-market-list";
