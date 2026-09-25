c69017406f3426b3c86a3e3b68e381ae5f772cda
stevesuzon <appli.suzon@gmail.com>
2026-09-25 22:19:07 +0200

    Rollback voix V487: renouveler le cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const VERSION = "V504-V489-RESTORED";
-const BASE_CACHE = "couteau-suisse-runtime-code-v504";
+const VERSION = "V487-ROLLBACK-STABLE";
+const BASE_CACHE = "couteau-suisse-runtime-code-v487-rollback";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v439-direct";
 const ADMIN_CACHE = "couteau-suisse-module-admin-v439-market-list";
