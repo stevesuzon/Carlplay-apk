addeca97abfcf47e128353f167dc8d6bded7cb61
stevesuzon <appli.suzon@gmail.com>
2026-09-25 21:39:02 +0200

    V499: renouveler le cache vocal

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const VERSION = "V498-LONG-FIRST-PRESS";
-const BASE_CACHE = "couteau-suisse-runtime-code-v498";
+const VERSION = "V499-LONG-PRESS-FIX";
+const BASE_CACHE = "couteau-suisse-runtime-code-v499";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v439-direct";
 const ADMIN_CACHE = "couteau-suisse-module-admin-v439-market-list";
