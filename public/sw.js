c50b9d3593bdad7e458da55493542d365bb9670f
stevesuzon <appli.suzon@gmail.com>
2026-09-25 10:23:10 +0200

    Show dynamic referral celebration V464: public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "V463-REFERRAL-REWARDS";
+const VERSION = "V464-REFERRAL-CELEBRATION";
 const BASE_CACHE = "couteau-suisse-runtime-code-v1";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v439-direct";
