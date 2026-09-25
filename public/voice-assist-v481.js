c9bb2d968b2e017b0757f6d51905b6da972a268e
stevesuzon <appli.suzon@gmail.com>
2026-09-25 21:14:28 +0200

    V495: charger le moteur vocal iPhone simplifié

--- public/voice-assist-v481.js
+++ public/voice-assist-v481.js
@@ -1,1 +1,1 @@
-(function(){if(window.__carplayVoiceV486Loaded)return;var s=document.createElement('script');s.src='/voice-assist-v494.js?v=494-new-file';s.defer=true;(document.head||document.documentElement).appendChild(s)})();
+(function(){if(window.__carplayVoiceV486Loaded)return;var s=document.createElement('script');s.src='/voice-assist-v495.js?v=495-simple-iphone';s.defer=true;(document.head||document.documentElement).appendChild(s)})();
