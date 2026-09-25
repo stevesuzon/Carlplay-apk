7363b7a550e640dcb8aba2b367038fc8b70f52b6
stevesuzon <appli.suzon@gmail.com>
2026-09-25 21:20:03 +0200

    V496: renouveler le cache voix page par page

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const VERSION = "V495-SIMPLE-VOICE";
-const BASE_CACHE = "couteau-suisse-runtime-code-v495";
+const VERSION = "V496-PAGE-FIRST-TAP";
+const BASE_CACHE = "couteau-suisse-runtime-code-v496";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v439-direct";
 const ADMIN_CACHE = "couteau-suisse-module-admin-v439-market-list";
