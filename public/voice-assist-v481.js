761f7cf1fead8f3f1f57dc5b5fd31d3fdc7b2595
stevesuzon <appli.suzon@gmail.com>
2026-09-25 19:21:41 +0200

    V486 neutralise les anciens moteurs vocaux

--- public/voice-assist-v481.js
+++ public/voice-assist-v481.js
@@ -1,1 +1,1 @@
-(function(){if(window.__couteauVoiceV485Loaded)return;var s=document.createElement('script');s.src='/voice-assist-v485.js?v=485-ios-release';s.defer=true;(document.head||document.documentElement).appendChild(s)})();
+(function(){if(window.__carplayVoiceV486Loaded)return;var s=document.createElement('script');s.src='/voice-assist-v486.js?v=486-old-ios-engine';s.defer=true;(document.head||document.documentElement).appendChild(s)})();
