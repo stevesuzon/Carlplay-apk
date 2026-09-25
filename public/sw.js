f5f1241d18cd7087ae8235e6abcd3a0a97efb160
stevesuzon <appli.suzon@gmail.com>
2026-09-25 14:11:12 +0200

    V475 fiabilise le deploiement Cloudflare et force le nouveau cache voix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const VERSION = "V474-VOICE-ONCE-NEAREST5";
+const VERSION = "V475-VOICE-ONCE-NEAREST5";
 const BASE_CACHE = "couteau-suisse-runtime-code-v1";
 const ICON_CACHE = "couteau-suisse-module-icons-v283";
 const CONTEST_CACHE = "couteau-suisse-module-contest-v439-direct";
